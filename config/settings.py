084e1f4be3232f6cf274f0393eb263bf8b094d0b
GS-Manoj-Kumar <147612752+GS-Manoj-Kumar@users.noreply.github.com>
2026-03-19 21:12:19 +0530

    debug allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
