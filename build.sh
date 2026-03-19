c69d1db88e01d52e2d2300d82ece0fa79d988397
GS-Manoj-Kumar <147612752+GS-Manoj-Kumar@users.noreply.github.com>
2026-03-19 22:20:09 +0530

    created super user

--- build.sh
+++ build.sh
@@ -1,8 +1,27 @@
+# #!/usr/bin/env bash
+# set -o errexit
+# pip install -r requirements.txt
+# python manage.py collectstatic --noinput
+# python manage.py migrate
+
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
 
 
 # Then push it to GitHub:
